0ad74584854ab164b2039be901369768f676a5c1
dabisin04 <campomunozd13@gmail.com>
2025-05-04 17:00:16 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -32,7 +32,5 @@ def index():
 
 # 🚀 Ejecutar servidor
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # Crear tablas si no existen
     app.run(debug=True, port=5050, host="0.0.0.0")
 
